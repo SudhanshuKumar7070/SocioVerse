2827f685814d86064ea1dbe468aaf78b7a0e0561
sk <you@example.com>
2025-09-23 21:00:37 +0530

    commit changes at Start_Chat_Page- deed some refinements over it

---
+++ Frontend/chatApp/src/components/ChatSpacing/Start_Chat_Page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Start_Chat_Page() {
+  return (
+    <div className='min-w-full min-h-[100vh]  flex justify-center items-center p-12'>
+      <h1 className=' text-neutral-400 text-5xl font-poppins  leading-tight tracking-tight text-center  max-w-3xl '>Click on User profile to start the chat</h1>
+    </div>
+  )
+}
+
+export default Start_Chat_Page
