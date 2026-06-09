ebea0ea8b42b9e94bd72be773bf7964e79355c77
sk <you@example.com>
2026-06-10 04:06:07 +0530

    fix: url in backend/server/app.js

--- Backend/server/app.js
+++ Backend/server/app.js
@@ -22,8 +22,8 @@ app.use(express.json({limit:"10kb"}));
 app.use(cookieParser());
 app.use(cors(
      {
-         origin:"http://localhost:5173"
-         , credentials:true
+         origin: process.env.CORS_ORIGIN,
+         credentials:true
      }
 ))
 app.use("/api/v1/auth",authRoutes);
